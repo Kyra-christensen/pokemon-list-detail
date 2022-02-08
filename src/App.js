d5c896930277e1e6bb00d731ba5a00ea9f50f639
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-08 14:57:22 -0800

    imported react-router-dom into app.js, and set up app.js so i can insert components into the routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route>
+            {/*This links back to home page, should list all pokemon*/}
+
+          </Route>
+          <Route>
+            {/*This points to a particular pokemon by id and render that specific pokemonDetail page*/}
+            
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
