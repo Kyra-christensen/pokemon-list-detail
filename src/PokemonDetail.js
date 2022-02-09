c10e1dbb007d383c74eb060e6ab460f35c3ffe12
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-08 17:13:10 -0800

    imported files that i will need for the rest of the page

---
+++ src/PokemonDetail.js
@@ -0,0 +1,5 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { getSinglePokemon } from './services/fetch-utils';
+
