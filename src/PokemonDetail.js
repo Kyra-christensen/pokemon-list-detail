15ff710270a17c4eb0f6553d46a28684b76e395e
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-08 17:16:23 -0800

    added state and useEffect function

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -3,3 +3,18 @@ import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { getSinglePokemon } from './services/fetch-utils';
 
+export default function PokemonDetail() {
+  const [singlePokemon, setSinglePokemon] = useState('');
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const data = await getSinglePokemon(params.id);
+
+      setSinglePokemon(data);
+    }
+    fetchPokemon();
+  }, [params.id]);
+
+  
+}
