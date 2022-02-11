320fd1953952f193eda3198c4e71340ee8a5257b
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-11 00:51:43 -0800

    class component refactor on the pokemonList array

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,9 +1,23 @@
 import Pokemon from './Pokemon';
+import { Component } from 'react';
 
-export default function PokemonList({ pokemonArray }) {
-  return (
-    <div className='pokemon'>
-      {pokemonArray.map((pokemon, i) => <Pokemon key={`${pokemon.id}-${i}`} pokemon={pokemon} />)}
-    </div>
-  );
+// export default function PokemonList({ pokemonArray }) {
+//   return (
+//     <div className='pokemon'>
+//       {pokemonArray.map((pokemon, i) => <Pokemon key={`${pokemon.id}-${i}`} pokemon={pokemon} />)}
+//     </div>
+//   );
+// }
+
+export default class PokemonList extends Component {
+  render() {
+    const { pokemonArray } = this.props;
+    return (
+      <>
+        <div className='pokemon'>
+          {pokemonArray.map((pokemon, i) => <Pokemon key={`${pokemon.id}-${i}`} pokemon={pokemon} />)}
+        </div>
+      </>
+    );
+  }
 }
